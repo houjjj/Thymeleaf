83b2e229b7120338e749712b9e1783e9d4b63842
HouJun <mrhjgg@gmail.com>
2021-12-22 11:29:47 +0800

    add module

---
+++ Think/src/main/java/com/houjun/dp/Main.java
@@ -0,0 +1,11 @@
+package com.houjun.dp;
+
+/**
+ * @author HouJun
+ * @date 2021-12-22 11:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
